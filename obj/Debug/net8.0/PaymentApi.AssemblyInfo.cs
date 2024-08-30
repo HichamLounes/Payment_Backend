975367f75384321fb892e639f484380ce171056f
Hicham Lounes <lounesh1@gmail.com>
2024-08-30 23:55:59 +0100

    create payment details controller

--- obj/Debug/net8.0/PaymentApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/PaymentApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff6df4e43bebff66b969f018e9fbe16ad5ccd81")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
